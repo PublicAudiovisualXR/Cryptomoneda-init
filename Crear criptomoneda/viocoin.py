cfcd87d47086e23daf5aa8e353aadb1143686e4a
Jordi Martos <develop@publicaudiovisual.com>
2021-12-10 09:18:16 +0100

    Update viocoin.py

--- Crear criptomoneda/viocoin.py
+++ Crear criptomoneda/viocoin.py
@@ -27,13 +27,16 @@ class Blockchain:
     
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(proof = 1, previous_hash = '0')
         
     def create_block(self, proof, previous_hash):
         block = {'index' : len(self.chain)+1,
                  'timestamp' : str(datetime.datetime.now()),
                  'proof' : proof,
-                 'previous_hash' : previous_hash}
+                 'previous_hash' : previous_hash
+                 'transactions': self.transactions}
+        self.transactions = []
         self.chain.append(block)
         return block
     
@@ -70,6 +73,13 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+    
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({'sender': sender,
+                                  'receiver' : receiver,
+                                  'amount' : amount})
+        previous_block = self.previous_block()
+        return previous_block['index'] + 1
             
 # Parte 2 - Minado de un bloque de la cadena
 
